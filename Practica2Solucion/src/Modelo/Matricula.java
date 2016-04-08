c188d0b9db5cb570dbafa490b5f6b36e4de965c9
llasu <llasu@llasu-PC>
2016-04-08 09:25:17 -0600

    objetoMatricula

--- Practica2Solucion/src/Modelo/Matricula.java
+++ Practica2Solucion/src/Modelo/Matricula.java
@@ -11,4 +11,34 @@ package Modelo;
  */
 public class Matricula {
     
-}
+    String carnet;
+    String sigla;
+    int  contadorEstudiante;
+
+    public String getCarnet() {
+        return carnet;
+    }
+
+    public void setCarnet(String carnet) {
+        this.carnet = carnet;
+    }
+
+    public String getSigla() {
+        return sigla;
+    }
+
+    public void setSigla(String sigla) {
+        this.sigla = sigla;
+    }
+
+    public int getContadorEstudiante() {
+        return contadorEstudiante;
+    }
+
+    public void setContadorEstudiante(int contadorEstudiante) {
+        this.contadorEstudiante = contadorEstudiante;
+    }
+    
+    
+    
+}//Fin de la clase objeto
